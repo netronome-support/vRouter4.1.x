2b105983a7adfebee88fd0c0a7e1ae335bc75898
netronome-support <netronome-support@netronome.com>
2018-05-31 11:07:09 +0000

    Commit 001 Nick.vonklemperer@netronome.com Autofills image.json script from conf templates

---
+++ scripts/07_load_image.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Loads Ocata openstack image
+#TODO: Add check for existing image and remove from server-manager
+echo "Making JSON config directory"
+cd ${PWD}/../
+if [ $(ls | grep "confs") == "confs" ]; then
+	rm -r confs/*
+else
+	mkdir confs
+fi
+
+echo "Copying and updating config templates"
+cp ${PWD}/clusterconf/2node_cluster/* ${PWD}/confs
+IMAGE="${PWD}/confs/image.json"
+DIR='s,<docker_path>,'${PWD}'/contrail-cloud-docker_4.1.0.0-8-ocata_xenial.tgz,g'
+sed -i $DIR $IMAGE
+
+echo "Downloading OpenStack Image"
+wget http://172.26.1.131/nas/vrouter4/contrail-cloud-docker_4.1.0.0-8-ocata_xenial.tgz
+
+echo "Adding image to server manager"
+server-manager add image -f $IMAGE
+
+echo "Waiting for image upload"
+LAST="start"
+while [ $(docker image list | grep main | wc -l) != "57" ]; do
+	CURRENT=$(docker image list | grep main | wc -l)
+	if [ "${CURRENT}" != "${LAST}" ]; then 
+		echo  "Docker image no: $CURRENT"
+		LAST=$CURRENT
+	fi
+done
+echo "Instalation complete"
