a8ecb18110a721dafb4f1fa3639b5f57a94bfa96
netronome-support <netronome-support@netronome.com>
2018-06-04 10:03:23 +0000

    Commit 008 Nick.vonklemperer@netronome.com Changed nova reset

--- scripts/utils/02_reset_nova.sh
+++ scripts/utils/02_reset_nova.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 CNTRL_IP=$1
 echo "Restarting Nova on: $CNTRL_IP"
+sleep 5
 echo "Stopping ns-vrouter"
 ssh $CNTRL_IP 'ns-vrouter-ctl stop'
+sleep 5
 echo "Stopping contrail vrouter agent"
 ssh $CNTRL_IP 'Service contrail-vrouter-agent stop'
+sleep 5
 echo "Restarting ns-vrouter"
 ssh $CNTRL_IP 'ns-vrouter-ctl restart'
+sleep 5
 echo "Restarting networking"
 ssh $CNTRL_IP 'service networking restart'
+sleep 5
 echo "Restarting nova compute"
 ssh $CNTRL_IP 'systemctl restart nova-compute'
+sleep 5
 echo "Service contrail-vrouter-agent start"
 ssh $CNTRL_IP 'service contrail-vrouter-agent start'
