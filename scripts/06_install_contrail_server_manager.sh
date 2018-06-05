659e5a9e7d145d659c0dbc2ba22b12e53d252989
netronome-support <netronome-support@netronome.com>
2018-06-05 05:48:54 +0000

    Commit 010 Nick.vonklemperer@netronome.com Corrected cluster provisioning issues

--- scripts/06_install_contrail_server_manager.sh
+++ scripts/06_install_contrail_server_manager.sh
@@ -11,10 +11,9 @@ dpkg -i contrail-server-manager-installer_4.1.0.0-8~xenial.deb
 # Start contrail setup and watch it's deployment
 echo "Starting contrail install"
 /opt/contrail/contrail_server_manager/setup.sh --all --smlite --hostip=172.26.1.53
-
-tail -f `find /var/log/contrail/install_logs | sort -r | head -1`
+tail -f `find /var/log/contrail/install_logs | sort -r | head -1` | tee /dev/tty | awk '/SM installation took/ {system("pkill tail")}'
 # Restart contrail service
 echo "Restarting contrail"
 service contrail-server-manager restart
 echo "Cleaning up"
-rm -r contrail-server-manager-nstaller_4.1.0.0-8~xenial.deb
+rm -r contrail-server-manager-installer_4.1.0.0-8~xenial.deb
--- scripts/09_load_cluster.sh
+++ scripts/09_load_cluster.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Creates cluster in server-manager
 #TODO: Add check for existing image and remove from server-manager
-cl_nm=atari_apple
-dm=netronome.com
-gway=172.26.0.1
-msk=255.255.252.0
-cntrl=atari
-cntrl_dm=$cntrl.$dm
-cmp=apple
-cmp_dm=$cmp.$dm
-ovc_ip=172.26.1.51
-cmp_ip=172.26.1.52
+cl_nm="atari_apple"
+dm="netronome.com"
+gway="172.26.0.1"
+msk="255.255.252.0"
+cntrl="atari"
+cntrl_dm="$cntrl.$dm"
+cmp="apple"
+cmp_dm="$cmp.$dm"
+ovc_ip="172.26.1.51"
+cmp_ip="172.26.1.52"
 echo "Checking for config directory"
 cd ${PWD}/../
 if [ $(ls | grep "confs") == "confs" ]; then
@@ -29,13 +29,13 @@ DIR='s,<dm>,'$dm',g'
 sed -i $DIR $CLUSTER
 DIR='s,<gway>,'$gway',g'
 sed -i $DIR $CLUSTER
-DIR='s,<msk>,'$gway',g'
+DIR='s,<msk>,'$msk',g'
 sed -i $DIR $CLUSTER
 DIR='s,<cntrl>,'$cntrl',g'
 sed -i $DIR $CLUSTER
 DIR='s,<cntrl_dm>,'$cntrl_dm',g'
 sed -i $DIR $CLUSTER
-DIR='s,<ovc_ip>,'$cntrl_ip',g'
+DIR='s,<ovc_ip>,'$ovc_ip',g'
 sed -i $DIR $CLUSTER
 DIR='s,<cmp>,'$cmp',g'
 sed -i $DIR $CLUSTER
